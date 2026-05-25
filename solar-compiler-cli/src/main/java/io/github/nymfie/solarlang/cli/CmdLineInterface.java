cbb8a1841bad2b9c4f72f32e01136b981aa08940
Nym <60491206+nymfie@users.noreply.github.com>
2026-05-25 16:51:41 -0400

    Initial commit

---
+++ solar-compiler-cli/src/main/java/io/github/nymfie/solarlang/cli/CmdLineInterface.java
@@ -0,0 +1,9 @@
+package io.github.nymfie.solarlang.cli;
+
+public class CmdLineInterface {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
---
+++ solar-compiler-lib/src/main/java/io/github/nymfie/solarlang/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.nymfie.solarlang;
+
+import org.jspecify.annotations.NullMarked;
