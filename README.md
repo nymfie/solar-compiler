5a5824294b693d82004eb6d615439b18be2f2c90
Nym <60491206+nymfie@users.noreply.github.com>
2026-05-27 14:23:12 -0400

    Create SourceScribe project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Solarlang Compiler Project
+
+## Attributions
+
+This project contains a partial port of the [annotate-snippets](https://github.com/rust-lang/annotate-snippets-rs) Rust crate, under the [sourcescribe](sourcescribe/) folder.
+
+The full license for its MIT license is available at [sourcescribe/LICENSE](sourcescribe/LICENSE)
---
+++ sourcescribe/src/main/java/io/github/nymfie/sourcescribe/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Rough reimplementation of Rust's "annotate_snippets" crate for amateur use<br>
+ * <br>
+ * Package is <a href="https://jspecify.dev/docs/api/org/jspecify/annotations/NullMarked.html">null-marked</a>
+ * 
+ * @see <a href=
+ *      "https://docs.rs/annotate-snippets/latest/annotate_snippets/index.html">Documentation
+ *      for <code>annotate_snippets</code></a>
+ */
+@NullMarked
+package io.github.nymfie.sourcescribe;
+
+import org.jspecify.annotations.NullMarked;
